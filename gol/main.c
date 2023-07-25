11ff062f886ee0fe15dafb02d11496f9995d371f
rastko <rastko.vukasinovic@gmail.com>
2023-07-25 22:29:17 +0200

    The setup

---
+++ gol/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ROWS 5
+#define COLS 5
+
+int world[ROWS][COLS] = {
+    {0,0,1,0,1},
+    {0,0,1,0,1},
+    {1,0,1,0,1},
+    {0,1,0,1,0},
+    {0,0,0,0,0}
+};
+
+int main() {
+
+    return 0;
+}
