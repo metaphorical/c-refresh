9023fb4e8264c80607f74f40bce445cbed5fd755
rastko <rastko.vukasinovic@gmail.com>
2023-08-02 09:28:49 +0200

    Getting ready to extrat world library

--- gol/lib/files.h
+++ gol/lib/files.h
@@ -1,7 +1,11 @@
+/*
+    writing and reading stuff from disc
+*/
+
 #ifndef FILES_H // Include guard to prevent multiple inclusion
 #define FILES_H
 
-// Function prototypes
+// Function prototype
 void save_world_to_disc(const char* filename, int world[ROWS][COLS]);
 
 #endif // FILES_H 
---
+++ gol/lib/world.h
@@ -0,0 +1,8 @@
+/*
+    world library for Game of Life
+*/
+
+#ifndef WORLD_H // Include guard to prevent multiple inclusion
+#define WORLD_H
+
+#endif // WORLD_H 
